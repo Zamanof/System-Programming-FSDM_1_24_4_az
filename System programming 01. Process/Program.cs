0351082da899b22f57dbef98f61a327a151b00a6
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-03-17 12:37:16 +0400

    Process

--- System programming 01. Process/Program.cs
+++ System programming 01. Process/Program.cs
@@ -1,1 +1,40 @@
-﻿P
+﻿using System.Diagnostics;
+
+//Process.Start("cmd");
+//Process.Start("calc");
+//Process.Start("mspaint");
+//Process.Start(@"C:\Program Files\Google\Chrome\Application\Chrome.exe");
+
+//Console.WriteLine(Process.GetCurrentProcess().ProcessName);
+//Console.WriteLine(Process.GetCurrentProcess().Id);
+
+//var process = Process.GetProcessById(17764); // Process Id (PID) OS terefinden dinamik olaraq verilir ve her defe ferqli ola biler
+//process.Kill();
+
+
+//var calcProcesses = Process.GetProcessesByName("CalculatorApp");
+
+//foreach (var process in calcProcesses)
+//{
+//    Console.WriteLine($"{process.Id} - {process.ProcessName} - {process.Threads.Count}");
+//    process.Kill();
+//}
+
+//var allProcesses = Process.GetProcesses();
+
+
+//foreach (var process in allProcesses)
+//{
+//    Console.WriteLine($"{process.Id} - {process.ProcessName} - {process.Threads.Count}");
+//}
+
+//var current = Process.GetCurrentProcess();
+
+//current.PriorityClass = ProcessPriorityClass.Normal;
+
+
+
+
+//ProcessStartInfo startInfo = new ProcessStartInfo("calc");
+//Process.Start(startInfo);
+Console.ReadLine();
