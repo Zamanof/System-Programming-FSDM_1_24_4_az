abef7dafd6804d40d6194cf39f41de5adc9afe44
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-04-02 12:27:15 +0400

    ThreadPool

---
+++ SP 03. Thread vs ThreadPool/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+int operationCount = 35500;
+var watch = new Stopwatch();
+
+watch.Start();
+UseThread(operationCount);
+watch.Stop();
+Console.WriteLine($"{watch.ElapsedMilliseconds} milliseconds.");
+watch.Reset();
+
+watch.Start();
+UseThreadPool(operationCount);
+watch.Stop();
+Console.WriteLine($"{watch.ElapsedMilliseconds} milliseconds.");
+watch.Reset();
+
+void UseThread(int operationCount)
+{
+    using (var count = new CountdownEvent(operationCount))
+    {
+        Console.WriteLine("Threads...");
+        for (int i = 0; i < operationCount; i++)
+        {
+            var thread = new Thread(() =>
+            {
+                Thread.Sleep(1);
+                //Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+                count.Signal();
+            });
+            thread.Start();
+        }
+        count.Wait();
+        Console.WriteLine();
+    }
+}
+
+void UseThreadPool(int operationCount)
+{
+    List<int> threadsIds = new();
+    using (var count = new CountdownEvent(operationCount))
+    {
+        Console.WriteLine("\tThreadPool...");
+        for (int i = 0; i < operationCount; i++)
+        {
+
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                Thread.Sleep(1);
+                //Console.WriteLine($"\tThread {Thread.CurrentThread.ManagedThreadId}");
+                if (!threadsIds.Contains(Thread.CurrentThread.ManagedThreadId))
+                {
+                    threadsIds.Add(Thread.CurrentThread.ManagedThreadId);
+                }
+                count.Signal();
+            });
+        }
+        count.Wait();
+        Console.WriteLine($"\t{threadsIds.Count}");
+    }
+}
---
+++ SP 03. ThreadPool/Program.cs
@@ -0,0 +1,37 @@
+﻿// ThreadPool
+//ThreadPool.GetAvailableThreads(out int workerCount, out int complCount);
+//Console.WriteLine(workerCount);
+//Console.WriteLine(complCount);
+
+
+Console.WriteLine("Main method start...");
+
+//Console.WriteLine($"\tMain isBackground: {Thread.CurrentThread.IsBackground}");
+//Console.WriteLine($"\tMain isThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+
+ThreadPool.QueueUserWorkItem(AsyncOperations, "Salam");
+ThreadPool.QueueUserWorkItem(o=> { SomeOperations(); });
+
+Console.ReadKey();
+Console.WriteLine("Main method end...");
+
+void AsyncOperations(object state)
+{
+    Console.WriteLine("\tAsyncOperations method start...");
+    Console.WriteLine($"\tState: {state}");
+    Console.WriteLine($"\tAsyncOperations thread id: {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine($"\tAsyncOperations isBackground: {Thread.CurrentThread.IsBackground}");
+    Console.WriteLine($"\tAsyncOperations isThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+    Console.WriteLine("\tAsyncOperations method end...");
+}
+
+void SomeOperations()
+{
+    Console.WriteLine("\t\tSomeOperations method start...");
+    Console.WriteLine($"\t\tSomeOperations thread id: {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine($"\t\tSomeOperations isBackground: {Thread.CurrentThread.IsBackground}");
+    Console.WriteLine($"\t\tSomeOperations isThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+    Console.WriteLine("\t\tSomeOperations method end...");
+}
+
+
